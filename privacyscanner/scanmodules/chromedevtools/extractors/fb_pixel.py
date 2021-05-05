6c1d5f10fde553b3becc947e78773d5ec6d108a9
Ronghao pan <ronghaopan2014@gmail.com>
2021-05-05 19:12:15 +0200

    Remove facebook pixel advanced match

--- privacyscanner/scanmodules/chromedevtools/extractors/fb_pixel.py
+++ privacyscanner/scanmodules/chromedevtools/extractors/fb_pixel.py
@@ -9,23 +9,6 @@ FB_STANDARD_EVENTS = {
     "SubmitApplication", "Subscribe", "ViewContent"
 }
 
-FB_ADVANCED_MATCHING_PARAMETERS = {
-  "ud[em]": "Email",
-  "ud[fn]": "First Name",
-  "ud[ln]": "Last Name",
-  "ud[ph]": "Phone",
-  "ud[ge]": "Gender",
-  "ud[db]": "Birthdate",
-  "ud[city]": "City",
-  "ud[ct]": "City",
-  "ud[state]": "State or Province",
-  "ud[st]": "State or Province",
-  "ud[zp]": "Zip Code",
-  "ud[cn]": "Country",
-  "ud[country]": "Country",
-  "ud[external_id]": "An ID from another database.",
-}
-
 
 class FacebookPixelExtractor(Extractor): 
     def extract_information(self):  
@@ -41,8 +24,7 @@ class FacebookPixelExtractor(Extractor):
                 fb_query = request['parsed_url'].query.split('&')
                 for ele in fb_query: 
                     res_stantard = [ev for ev in FB_STANDARD_EVENTS if(ev in ele)]
-                    res_advanced = [ev for ev in FB_ADVANCED_MATCHING_PARAMETERS.keys() if(ev in ele)]
-                    if res_stantard or res_advanced: 
+                    if res_stantard: 
                         fb['facebook_pixel'] = True
                         fb['event'].append(ele)
 
